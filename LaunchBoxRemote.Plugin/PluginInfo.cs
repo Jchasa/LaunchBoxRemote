2e5e5add6a79501292d495d47a8fdee7bcbdfd3f
Jchasa <chastainaz@hotmail.com>
2026-07-20 16:51:47 -0700

    Add PluginInfo class

---
+++ LaunchBoxRemote.Plugin/PluginInfo.cs
@@ -0,0 +1,8 @@
+namespace LaunchBoxRemote.Plugin;
+
+public static class PluginInfo
+{
+    public const string Name = "LaunchBox Remote";
+    public const string Version = "0.1.0";
+    public const string Author = "Josh Chastain & ChatGPT";
+}
