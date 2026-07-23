6fdb0df01705e1746559f7c9f3326dedd39a4361
Jchasa <chastainaz@hotmail.com>
2026-07-22 19:41:44 -0700

    Add initial RequestRouter scaffold

---
+++ LaunchBoxRemote.Plugin/RequestRouter.cs
@@ -0,0 +1,10 @@
+namespace LaunchBoxRemote.Plugin;
+
+public sealed class RequestRouter
+{
+    public void Route(object context)
+    {
+        Logger.Info("Routing incoming request.");
+        // TODO: Route API requests to controllers.
+    }
+}
